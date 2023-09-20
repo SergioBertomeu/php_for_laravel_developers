1e021205e3651eef8875c32904ca21c65d4d02ae
«SergioBertomeu» <sbertomeu1@iesebre.com>
2023-09-20 17:19:12 +0200

    Treball PHP Video 1

--- app/index.php
+++ app/index.php
@@ -2,7 +2,14 @@
 
 // QUERY STRING
 //var_dump($_GET['name']);
-$name = 'Sergio';
-$greeting = 'Hola ' . $_GET['name'] . '!';
 
-echo $greeting;  // Esto imprimirá: Hola Sergio!
+// API
+function greet(){
+    $name = htmlspecialchars($_GET['name']);
+    $surname = $_GET['surname'];
+    return  "Hola $name $surname !";
+
+    echo $greeting;  // Esto imprimirá: Hola Sergio!
+
+}
+$greeting = greet();
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,23 @@
 </head>
 <body>
 <header>
-<h1><?=$greeting;?></h1>
+    <h1><?=$greeting;?></h1>
+
+    <table>
+        <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Completed</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Estudiar PHP</td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+    
 </header>
 </body>
 </html>
