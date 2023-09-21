8fd811ddf9ecd64989d2acf52620cc43376f2bfd
«SergioBertomeu» <sbertomeu1@iesebre.com>
2023-09-21 19:48:11 +0200

    Neteja de comentaris

--- app/Task.php
+++ app/Task.php
@@ -1,15 +1,6 @@
 <?php
 class Task{
-protected $id;
-protected $title;
-protected $description;
-protected $completed;
+    public function __construct(){
 
-public function __construct($id, $title, $description, $completed )
-{
-$this->id = $id;
-$this->id = $title;;
-$this->id = $description;
-$this->id = $completed;
-}
+    }
 }
